54fc54b76bd905fd7d6118804cf3af16a8f07f43
Natu <cyy3661@naver.com>
2026-03-08 01:03:54 +0900

    0308 01:02 로그인 컨트롤러 구현/ 라우터 등록 / Login php폼

--- laravel-crud-app/routes/web.php
+++ laravel-crud-app/routes/web.php
@@ -1,20 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController; # 로그인 컨트롤러 경로 선언
+use Illuminate\Support\Facades\Route; # 라우트 메소드 정의하는 파사드
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/posts', function () {
+    return '로그인 성공 후 게시판 목록 페이지';
+})->middleware('auth'); # 이거인가다ㅏㅏㅏ, 접근 중간에 middleware 개념 획득했는지 확인
