3134c284dc9ced14b8558a566bb5181ed64a96be
Natu <cyy3661@naver.com>
2026-03-08 03:32:08 +0900

    0308, 03:30 게시글 목록+게시글 컨트롤러 / 목록, 생성, 글 상세보기 폼 구현

---
+++ laravel-crud-app/resources/views/posts/create.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>새 글 작성</title>
+</head>
+<body>
+    <h1>새 글 작성</h1>
+
+    @if ($errors->any())
+        <div style="color: red;">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        </div>
+    @endif
+
+    <form method="POST" action="{{ route('posts.store') }}">
+        @csrf
+
+        <div>
+            <label for="title">제목</label><br>
+            <input type="text" name="title" id="title" value="{{ old('title') }}" required>
+        </div>
+
+        <br>
+
+        <div>
+            <label for="content">내용</label><br>
+            <textarea name="content" id="content" rows="10" cols="50" required>{{ old('content') }}</textarea>
+        </div>
+
+        <br>
+
+        <button type="submit">등록</button>
+    </form>
+
+    <br>
+
+    <p>
+        <a href="{{ route('posts.index') }}">목록으로 돌아가기</a>
+    </p>
+</body>
+</html>
